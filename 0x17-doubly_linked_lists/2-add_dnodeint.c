a856b75a61c7e823a3a578729d2b1be01759d1fd
Eiden <eidnegabriel07@gmail.com>
2022-05-05 11:23:10 -0400

    added malloc if null

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return ('\0');
+	}
 
 	new->n = n;
 	new->prev = NULL;
