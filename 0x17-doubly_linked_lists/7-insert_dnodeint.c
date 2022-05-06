d5bea4ffd929fd1d2b69f6452977a793ff4c060e
Eiden <eidnegabriel07@gmail.com>
2022-05-06 15:38:58 -0400

    fix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,11 +42,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
-		add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 	}
 	if (idx == count)
 	{
-		add_dnodeint_end(h, n);
+		return (add_dnodeint_end(h, n));
 	}
 	return (new_node);
 }
